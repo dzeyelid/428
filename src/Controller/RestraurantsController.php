1d3ebcd6f3831ad8e7150cbcbd56484e0c5d6a05
Kazumi OHIRA <dzeyelid@gmail.com>
2018-03-24 15:59:13 +0900

    Updated ResraurantsController.

--- src/Controller/RestraurantsController.php
+++ src/Controller/RestraurantsController.php
@@ -3,8 +3,15 @@ namespace App\Controller;
 
 class RestraurantsController extends AppController
 {
+    public function initialize()
+    {
+        parent::initialize();
+        $this->loadComponent('RequestHandler');
+    }
+
     public function index()
     {
-        $this->set('_serialize', array('aaa', 'bbb'));
+        $this->set('data', ['aaa', 'bbb']);
+        $this->set('_serialize', 'data');
     }
 }
